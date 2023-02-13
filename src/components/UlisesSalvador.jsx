331a640c4c62796463afb9034e2411597ca39880
Ulices Salvador Aguila Contreras <Ulices13SAC@outlook.es>
2023-02-12 20:30:45 -0600

    config text

--- src/components/UlisesSalvador.jsx
+++ src/components/UlisesSalvador.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const UlisesSalvador = () => {
     return (
-        <div class="max-w-sm mx-2 bg-white shadow-lg rounded-lg overflow-hidden">
+        <div class="max-w-sm mx-2 bg-sky-700 shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)] rounded-lg overflow-hidden">
             <div class="sm:flex sm:items-center px-6 py-4">
                 <img
                 class="block mx-auto sm:mx-0 sm:flex-shrink-0 h-16 sm:h-24 rounded-full"
@@ -10,7 +10,7 @@ const UlisesSalvador = () => {
                 alt="Photo Profile"
             />
                 <div class="mt-4 sm:mt-0 sm:ml-4 text-center sm:text-left">
-                    <p class="text-xl leading-tight">Ulices Salvador</p>
+                    <p class="text-xl  leading-tight font-bold underline">Ulices Salvador</p>
                     <ul className='text-center'>
                         <li>Ir al gym</li>
                         <li>Videojuegos</li>
