f6f71c86c5a9423fe6ed1b7ad32af73e3b8ba6b7
Ulices Salvador Aguila Contreras <Ulices13SAC@outlook.es>
2023-02-12 20:12:41 -0600

    Style Ulices Component

--- src/components/UlisesSalvador.jsx
+++ src/components/UlisesSalvador.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const UlisesSalvador = () => {
     return (
-        <div>UlisesSalvador</div>
+        <div class="max-w-sm mx-2 bg-white shadow-lg rounded-lg overflow-hidden">
+            <div class="sm:flex sm:items-center px-6 py-4">
+                <img
+                class="block mx-auto sm:mx-0 sm:flex-shrink-0 h-16 sm:h-24 rounded-full"
+                src="https://picsum.photos/100/100"
+                alt="Photo Profile"
+            />
+                <div class="mt-4 sm:mt-0 sm:ml-4 text-center sm:text-left">
+                    <p class="text-xl leading-tight">Ulices Salvador</p>
+                    <ul className='text-center'>
+                        <li>Ir al gym</li>
+                        <li>Videojuegos</li>
+                        <li>Salir</li>
+                        <li>Ver series</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     )
 }
 
