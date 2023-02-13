92cfc789b456ab3c269a073a76f9269007f67082
Sebastian Castillo <sebascasti2019@gmail.com>
2023-02-12 21:10:44 -0500

    styles sebastian component

--- src/components/SebastianCastillo.jsx
+++ src/components/SebastianCastillo.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const SebastianCastillo = () => {
     return (
-        <div>SebastianCastillo</div>
+       <div className="container w-1/6 bg-[#002864] p-8 rounded-3xl m-2">
+
+        <h2 className="container_title text-center text-amber-100 p-3 font-serif text-lg">Sebastian Castillo Castillo</h2>
+        <h3 className="text-center bg-[#48a3ff] m-1 text-amber-100 rounded-2xl" >Hobbies</h3>
+        <ul className="container_list bg-purple-400 text-center m-1 text-amber-50 rounded-3xl font-serif font-bold">
+            
+            <li className="container_list p-4">Escuchar Musica</li>
+            <li className="container_list p-4">Programar</li>
+            <li className="container_list p-4">Leer</li>
+            <li className="container_list p-4">Caminar</li>
+
+        </ul>
+       </div>
     )
 }
 
